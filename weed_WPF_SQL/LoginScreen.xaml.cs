755565c80580b19586b04c89696bbd2969141609
Benjamin Lauwereins <benjamin.lauwereins@gmail.com>
2022-04-05 10:24:18 +0200

    Initial commit

---
+++ weed_WPF_SQL/LoginScreen.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace weed_WPF_SQL
+{
+    /// <summary>
+    /// Interaction logic for LoginScreen.xaml
+    /// </summary>
+    public partial class LoginScreen : Window
+    {
+        Character character = new Character();
+        MainMenu mainMenu = new MainMenu();
+        
+        public LoginScreen()
+        {
+            InitializeComponent();
+
+            character.Name = "John Doe";
+        }
+
+        public void CallMainMenu()
+        {
+            mainMenu.Show(character);
+            this.Close();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            CallMainMenu();
+        }
+    }
+}
---
+++ weed_WPF_SQL/MainMenu.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace weed_WPF_SQL
+{
+    /// <summary>
+    /// Interaction logic for MainMenu.xaml
+    /// </summary>
+    public partial class MainMenu : Window
+    {
+        //member variables
+        SellGame sellingGame = new SellGame();
+        FarmGame farmGame = new FarmGame();
+        Character character;
+
+        //Constructors
+        public MainMenu()
+        {
+            InitializeComponent();
+            
+        }
+        public MainMenu(Character obj)
+        {
+            character = obj;
+            InitializeComponent();
+        }
+        //Overloaded Show to pass our Character Object
+        public void Show(Character obj)
+        {
+            character = obj;
+            lbltest.Content = character.Name;
+            base.Show();
+        }
+
+        //member functions
+        public void CallSellingGame()
+        {
+            sellingGame.Show();
+        }
+        public void CallFarmGame()
+        {
+            farmGame.Show();
+        }
+        
+        public void GoBackToLogin()
+        {
+
+            LoginScreen backLogin = new LoginScreen();
+            backLogin.Show();
+            this.Close();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
